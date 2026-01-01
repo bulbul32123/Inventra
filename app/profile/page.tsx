c96ea0f03e48ce900ce53f39c2885aedf2a0b0d8
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2026-01-01 21:00:31 +0600

    updated

---
+++ app/profile/page.tsx
@@ -0,0 +1,21 @@
+import { DashboardLayout } from "@/components/layout/dashboard-layout";
+import ProfilePage from "@/components/profile/ProfilePage";
+
+export default function Profile() {
+  return (
+    <DashboardLayout>
+      <ProfilePage />
+    </DashboardLayout>
+  );
+}
+
+// Task for this project
+
+// 1. Build the profile page
+// 2. Update the Chart graph of the reports page
+// 3. Fix the barcode and SKU generator
+// 4. Build a Clean and modern Landing page for this project
+// 5. Try to add Account switch feature
+// 6. Create Categories like products
+// 7. In dashboard brings some components from the reports page
+// 8 Improve the UI
